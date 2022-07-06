14a0eb45bb1fee6a414bd23f8dda73883b0ad503
Joseph Barbone <JoeBarbone@aol.com>
2022-07-06 00:25:43 -0400

    all views are completed. Add dept completed.

--- server.js
+++ server.js
@@ -4,7 +4,23 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 
 
-console.log("XYZ Company - Employee Tracker");
+
+console.clear();
+
+console.log(` _______  __   __  _______  ___      _______  __   __  _______  _______ 
+|       ||  |_|  ||       ||   |    |       ||  | |  ||       ||       |
+|    ___||       ||    _  ||   |    |   _   ||  |_|  ||    ___||    ___|
+|   |___ |       ||   |_| ||   |    |  | |  ||       ||   |___ |   |___ 
+|    ___||       ||    ___||   |___ |  |_|  ||_     _||    ___||    ___|
+|   |___ | ||_|| ||   |    |       ||       |  |   |  |   |___ |   |___ 
+|_______||_|   |_||___|    |_______||_______|  |___|  |_______||_______|
+ __   __  _______  __    _  _______  _______  _______  ______           
+|  |_|  ||   _   ||  |  | ||   _   ||       ||       ||    _ |          
+|       ||  |_|  ||   |_| ||  |_|  ||    ___||    ___||   | ||          
+|       ||       ||       ||       ||   | __ |   |___ |   |_||_         
+|       ||       ||  _    ||       ||   ||  ||    ___||    __  |        
+| ||_|| ||   _   || | |   ||   _   ||   |_| ||   |___ |   |  | |        
+|_|   |_||__| |__||_|  |__||__| |__||_______||_______||___|  |_|        `);
 
 console.log("");
 console.log("");
@@ -29,7 +45,7 @@ const menuChoice = () => {
                 }
         ])
         .then(menuChoice => {
-                return runSwitch(menuChoice);
+                runSwitch(menuChoice);
         });
 
 };
@@ -44,34 +60,26 @@ function runSwitch(myMenuChoice) {
                 
 
                 case "View All Roles":
-                        console.log("View All Roles");
-                        viewAllRoles();
-                        break;
+                        return viewAllRoles();
+        
 
                 case "View All Employees":
-                        console.log("View All Employees");
-                        viewAllEmployees();
-                        break;
+                        return viewAllEmployees();
 
                 case "Add a Department":
-                        console.log("Add a Department");
-                        addDepartment();
-                        break;        
+                        return addDepartment();       
 
                 case "Add a Role":
-                        console.log("Add a role");
-                        break;        
+                        return addRole();
                 
                 case "Add an Employee":
-                        console.log("Add an Employee");
-                        break; 
-                        
+                        return addEmployee();
+
                 case "Update an Employee Role":
-                        console.log("Update an Employee Role");
-                        break;
+                        return addEmployeeRole();
                 
                 case "Exit":
-                        break;
+                        process.exit();
         }
  
 }
@@ -81,45 +89,42 @@ function runSwitch(myMenuChoice) {
 
 const viewAllDepts = () => {
 
-        //console.log("made it to view all departments function");
-        const sql = "SELECT * FROM tbl_dept";
+        const sql = "SELECT tbl_dept.id AS id, tbl_dept.dept_name AS name FROM tbl_dept";
         db.query(sql, function (err, results) {
-              if (err) throw err;
+                if (err) throw err;
                 
-              console.table([],results);
-        
+                console.table([],results);
+                menuChoice();
         });
         
-        //console.log("hello world!")
-        
-
 };
 
 
 
 const viewAllRoles = () => {
-        console.log("made it to view all roles function");
-        const sql = `SELECT tbl_role.*, tbl_dept.dept_name
-                AS deptname
-                FROM tbl_role
-                LEFT JOIN tbl_dept
-                ON tbl_role.dept_id = tbl_dept.id`;
+
+        const sql = `SELECT tbl_role.id AS id, tbl_role.title AS title, tbl_dept.dept_name AS department, tbl_role.salary as salary
+                FROM tbl_role 
+                LEFT JOIN tbl_dept 
+                ON tbl_role.dept_id = tbl_dept.id;`;
 
         db.query(sql, function (err, results) {
                 if (err) throw err;
                 
                 console.table([],results);
+                menuChoice();
         });
-        menuChoice();
+
 };
 
 
 
 const viewAllEmployees = () => {
+
         console.log("made it to view all roles function");
 
-        const sql = `SELECT tbl_emp.*, tbl_role.salary AS salary, tbl_role.title AS title, tbl_dept.dept_name 
-                AS department FROM tbl_emp 
+        const sql = `SELECT tbl_emp.id, tbl_emp.first_name, tbl_emp.last_name, tbl_role.title AS title, tbl_dept.dept_name AS department, tbl_role.salary AS salary 
+                FROM tbl_emp 
                 LEFT JOIN tbl_role ON tbl_emp.role_id = tbl_role.id 
                 LEFT JOIN tbl_dept ON tbl_emp.role_id = tbl_dept.id;`;
 
@@ -127,29 +132,163 @@ const viewAllEmployees = () => {
                 if (err) throw err;
                 
                 console.table([],results);
+                menuChoice();
         });
-        menuChoice();
+        
 };
 
 
 
 const addDepartment = () => {
 
-        const addDept = () => {
-        
-                return inquirer.prompt([
-                        {
-                                name: "deptName",
-                                message: "Enter name of new department",
-                                type: "input"
-                        }
-                ])
-                .then(addDept = () => {
-                        const sql = `INSERT INTO tbl_dept (?)`;
-                        db.query(sql,addDept);
-                });
-        };
+        console.log("made it to add department");
+
+        inquirer.prompt([
+                {
+                        name: "deptName",
+                        message: "Name of new department:",
+                        type: "input"
+                }
+        ])
+        .then(function (response) {
+
+                const sql = `INSERT INTO tbl_dept (dept_name) VALUES (?)`;
+                const newDept = response.deptName;
+                console.log(newDept);
+
+                db.query(sql, newDept, function (err) {
+                        if (err) throw err;
+
+                        console.log("New department added successfully!");
+                        console.log();
+                        console.log();
+                        menuChoice();
+                });                
+                
+        });
+
 };
+
+
+
+const addRole = () => {
+
+        const deptArr = getDeptList();
+
+        inquirer.prompt([
+                        
+                {
+                        name: "title",
+                        message: "Title of new role:",
+                        type: "input"
+                },
+                {
+                        name: "salary",
+                        message: "Salary of new role:",
+                        type: "input"
+                },
+                {
+                        name: "dept_id",
+                        message: "Department ID of new role:",
+                        type: "input"
+                },
+                {
+                        name: "dept_idx",
+                        message: "Select department of new role",
+                        type: "list",
+                        choices: deptArr
+                }
+        ])
+        .then(function (response) {
+
+                const sql = `INSERT INTO tbl_role (title, salary, dept_id) VALUES (?,?,?)`;
+
+                const newRoleArr = [response.title, response.salary, response.dept_id];
+
+                console.log(newRoleArr);
+
+                db.query(sql, newRoleArr, function (err) {
+                        if (err) throw err;
+
+                        console.log("New Role added successfully!");
+                        console.log();
+                        console.log();
+                        menuChoice();
+                });                
+                
+        });
+
+};
+
+
+
+const addEmployee = () => {
+
+        
+
+        inquirer.prompt([
+                {
+                        name: "first_name",
+                        message: "New employee first name:",
+                        type: "input"
+                },
+                {
+                        name: "last_name",
+                        message: "New employee last name:",
+                        type: "input"
+                },
+                {
+                        name: "role_id",
+                        message: "New employee role ID:",
+                        type: "input"
+                },
+                {
+                        name: "manager_id",
+                        message: "New employee manager ID:",
+                        type: "input"
+                }
+        ])
+        .then(function (response) {
+
+                const sql = `INSERT INTO tbl_emp (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+
+                const newEmployeeArr = [response.first_name, response.last_name, response.role_id, response.manager_id];
+
+                console.log(newEmployeeArr);
+
+                db.query(sql, newEmployeeArr, function (err) {
+                        if (err) throw err;
+
+                        console.log("New Employee added successfully!");
+                        console.log();
+                        console.log();
+                        menuChoice();
+                });                
+              
+        });
+
+};
+
+
+const getDeptList = () => {
+
+        const sql = `SELECT dept_name FROM tbl_dept`;
+        const tempArr = [];
+        db.query(sql, function (err, results) {
+                
+                if (err) throw err;
+                
+                for (let i=0; i < results.length; i++) {
+                        tempArr.push(results[i].dept_name);
+                };
+                
+        });
+
+        return tempArr;        
+
+}; 
+
+
 
 
 
